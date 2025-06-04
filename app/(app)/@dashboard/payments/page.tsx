7ff406b9ae54af503e5a2713d637bec2a2e462be
kirtank02 <kachhadiyakirtan1@gmail.com>
2025-06-04 12:52:50 +0530

    Your message

--- app/(app)/@dashboard/payments/page.tsx
+++ app/(app)/@dashboard/payments/page.tsx
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
-import { PaymentsTemplate } from "@/modules/payments/templates/payments-template";
-import { Metadata } from "next";
-=======
 import { PaymentsTemplate } from "@/modules/payments/templates/payments-template"
 import type { Metadata } from "next"
->>>>>>> d61e53c (Update payment history page UI and fix errors)
 
 export const metadata: Metadata = {
    title: "Sports Booking | Payment History",
    description: "Track earnings, refunds, commissions",
-<<<<<<< HEAD
-};
-
-export default function PaymentsPage() {
-   return <PaymentsTemplate />;
-} 
-=======
 }
 
 export default function PaymentsPage() {
    return <PaymentsTemplate />
 }
->>>>>>> d61e53c (Update payment history page UI and fix errors)
